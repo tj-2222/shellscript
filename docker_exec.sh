dedf2f0c8554e1b56082cbcd1931ec007237e3b5
tj2222 <takahiro.jukei@gmail.com>
2024-05-03 17:37:27 +0900

    checked: v2

--- docker_exec.sh
+++ docker_exec.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/bash
 
-# select container
-PS3="Which container number? (Enter 'q' for exit.)> "
+function selector() {
+  menu=$1
+  select item in $menu; do
+    if [ -n "${item}" ]; then
+      break
+    else
+      echo "invalid selection."
+      echo "exit."
+      exit 0
+    fi
+  done
+}
+
+# コンテナ選択
+PS3="Which container? > "
 menu="$(docker ps | tail -n +2 | rev | cut -d" " -f1 | rev)"
 item=""
-select item in $menu; do
-  # If enter 'q', exit this shell.
-  if [ "${REPLY}" = "q" ]; then
-    echo "exit."
-    exit 0
-  fi
+selector "$menu"
+container=$item
 
-  if [ -n "${item}" ]; then
-    break
-  else
-    echo "invalid selection."
-    echo "exit."
-    exit 0
-  fi
-done
+# 改行
+echo ""
 
-# select command
+# 起動コマンド選択
+PS3="Which command? > "
+menu='bash sh'
+item=""
+selector "$menu"
+command=$item
 
-docker exec -it "${item}" sh
+echo -e "Executing... 'docker exec -it ${container} ${command}'\n"
+
+docker exec -it "${container}" "${command}"
